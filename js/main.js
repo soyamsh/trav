59786cce028c4abb8d25d2470537c8f6af8bf5a5
soyamsh <124097954+soyamsh@users.noreply.github.com>
2023-03-25 14:23:21 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// TRAVERSE DATA CYU ASSIGNMENT START CODE
+
+// Load Data From Files
+let surveyData;
+fetch("data/survey-results.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (surveyData = strData.split(/\r?\n/)));
+
+let ageData;
+fetch("data/age-data.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (ageData = strData.split(/\r?\n/)));
+
+let numberData;
+fetch("data/number-data.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (numberData = strData.split(/\r?\n/)));
+
+// Output Element Variable
+let outputEl = document.getElementById("output");
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Get Menu Selection
+  let selection = document.getElementById("menu-select").value;
+
+  // Process Menu Selection
+  if (selection === "survey") {
+    traverseSurveyData();
+  } else if (selection === "ages") {
+    traverseAgeData();
+  } else if (selection === "numbers") {
+    traverseNumberData();
+  }
+}
+
+// Menu Option Functions
+function traverseSurveyData() {
+  // Traverse the surveyData array to:
+  // Count the number of "Yes" responses,
+  // Count the number of "No" responses,
+  // Count the number of "Maybe" responses,
+  // and output the results in the outputEl.
+
+  outputEl.innerHTML = "Survey Data";
+  console.log(surveyData);
+}
+
+function traverseAgeData() {
+  // Traverse the ageData array to:
+  // Count the number of ages under 18,
+  // Count the number of ages between 18 and 35, inclusive
+  // Count the number of ages between 36 and 65, inclusive
+  // Count the number of ages above 65,
+  // and output the results in the outputEl.
+
+  outputEl.innerHTML = "Age Data";
+  console.log(ageData);
+}
+
+function traverseNumberData() {
+  // Traverse the numberData array to:
+  // Count the number of even numbers,
+  // Count the number of odd numbers,
+  // and output the results in the outputEl.
+
+  outputEl.innerHTML = "Number Data";
+  console.log(numberData);
+}
---
+++ js/random-library.js
@@ -0,0 +1,19 @@
+// Random Library
+
+// Return a random decimbal b/t low (inclusive) and high (exclusive)
+function randomDec(low, high) {
+  return Math.random() * (high - low) + low;
+}
+
+// Return a random integer b/t low (inclusive) and high (exclusive)
+function randomInt(low, high) {
+  return Math.floor(randomDec(low, high));
+}
+
+// Return a random rgb color- 'rgb(__, __, __)'
+function randomRGB() {
+  return `rgb(${randomInt(0, 256)}, ${randomInt(0, 256)}, ${randomInt(
+    0,
+    256
+  )})`;
+}
